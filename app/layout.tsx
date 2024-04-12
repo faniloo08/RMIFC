fe122dc66e85a3c57088a4738846f3da30e8e785
Fanilo Rabemanantsoa <93961550+faniloo08@users.noreply.github.com>
2024-04-12 09:23:38 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ColorSwitcher from "./color-switcher/ColorSwitcher";
+import { cn } from "@/lib/utils"
+
+// const inter = Inter({ subsets: ["latin"] });
+import { Inter as FontSans } from "next/font/google"
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+
+export const metadata: Metadata = {
+  title: `RMIFC | Regional Maritime Informations Fusion Center`,
+  description: "RMIFC official website",
+  icons: {
+    icon: 'rmifc.ico'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}
+    >{children}
+    </body>
+    {/* <ColorSwitcher/> */}
+    </html>
+    
+  );
+}
